654e2d4a646d1a0a0fe0f90bc8a7ae63dacdbb58
Luciano Santos <luciano_z7@hotmail.com>
2019-02-16 11:51:55 -0200

    Created my-nav component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MyNavComponent } from './my-nav/my-nav.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  },
+  {
+    path: 'nav',
+    component: MyNavComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
