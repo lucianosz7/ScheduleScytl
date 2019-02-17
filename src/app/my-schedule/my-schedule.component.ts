0583cd3acc27bc5e8742ff3ff6460ae1a8092884
Luciano Santos <luciano_z7@hotmail.com>
2019-02-17 16:54:20 -0300

    Created mySchedule component

---
+++ src/app/my-schedule/my-schedule.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { MatMenuModule } from '@angular/material/menu';
+
+@Component({
+  selector: 'app-my-schedule',
+  templateUrl: './my-schedule.component.html',
+  styleUrls: ['./my-schedule.component.css']
+})
+export class MyScheduleComponent{
+
+  typesOfShoes: string[] = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches)
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  
+}
